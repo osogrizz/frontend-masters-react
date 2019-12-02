7b4145d01425c59e0700305237c2762186ab5add
osogrizz <lhtor@gmail.com>
2019-12-01 21:05:53 -0800

    Adds custom hook for dropdowns.

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,11 +1,12 @@
 import React, { useState } from "react";
 import { ANIMALS } from "@frontendmasters/pet";
+import useDropDown from "./useDropDown";
 
 const SearchParams = () => {
   const [location, setLocation] = useState("Seattle, WA");
-  const [animal, setAnimal] = useState("Dog");
-  const [breed, setBreed] = useState("");
   const [breeds, setBreeds] = useState([]);
+  const [animal, AnimalDropDown] = useDropDown("Animal", "dog", ANIMALS);
+  const [breed, BreedDropDown] = useDropDown("Breed", "", breeds);
 
   const handleChange = e => {
     e.preventDefault();
@@ -30,41 +31,8 @@ const SearchParams = () => {
             onChange={handleChange}
           />
         </label>
-        <label htmlFor="animal">
-          Animal
-          <select
-            id="animal"
-            value={animal}
-            placeholder="Animal"
-            onChange={e => setAnimal(e.target.value)}
-            onBlur={e => setAnimal(e.target.value)}
-          >
-            <option>All</option>
-            {ANIMALS.map(animal => (
-              <option key={animal} value="animal">
-                {animal}
-              </option>
-            ))}
-          </select>
-        </label>
-        <label htmlFor="breed">
-          Breed
-          <select
-            id="breed"
-            value={breed}
-            placeholder="Breed"
-            onChange={e => setBreed(e.target.value)}
-            onBlur={e => setBreed(e.target.value)}
-            disabled={!breeds.length}
-          >
-            <option>All</option>
-            {breeds.map(breedName => (
-              <option key={breedName} value={breedName}>
-                {breedName}
-              </option>
-            ))}
-          </select>
-        </label>
+        <AnimalDropDown />
+        <BreedDropDown />
         <button>Submit</button>
       </form>
     </div>
